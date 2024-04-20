af5ee13a14dc3c3048d33ebce0722276eb852167
Naren2002 <nompraka@andrew.cmu.edu>
2024-04-20 04:31:02 +0000

    Takes semantic simlarity into account when taking the best generated sentence

--- prompt_generation/experiment.py
+++ prompt_generation/experiment.py
@@ -141,6 +141,7 @@ if __name__ == "__main__":
     starting_pair = attack_df.iloc[0]["pairs"]
     used_pairs[starting_pair] = 1
 
+    best_generated_df = pd.DataFrame(columns = ["pairs", "loss", "sen_sim"])
     generated_df = pd.DataFrame(columns = ["pairs", "loss", "sen_sim"])
 
     for i in tqdm.tqdm(range(args.num_iterations)):
@@ -179,12 +180,27 @@ if __name__ == "__main__":
                                             args.num_shot,
                                             new_examples)
 
+      temp_df = pd.DataFrame(columns = ["pairs", "loss", "sen_sim"])
+
+      for gen_pairs_idx in range(len(example_loss_list)):
+        gen_pair = example_loss_list[gen_pairs_idx][0]
+        sen_sim_for_gen_pair = helpers.similarity_score(gen_pair[0], gen_pair[1])
+        temp_df.loc[len(temp_df)] = {"pairs": gen_pair, "loss": example_loss_list[gen_pairs_idx][1], "sen_sim": sen_sim_for_gen_pair}
+
+      helpers.normalize_column(temp_df, "loss")
+
+      temp_df["score"] = temp_df["normalized_loss"] + temp_df["sen_sim"]
+      temp_df.sort_values(by = ["score"], inplace = True)
+
       # If the starting pair does not have the best loss, pick the sentence pair with the lowest loss and append it to the attack_df
-      if best_idx != 0:
-        best_pairs = example_loss_list[best_idx][0]
+      if temp_df.iloc[0]["pairs"] != example_loss_list[0][0]:
+        print(temp_df.iloc[0]["pairs"])
+        print(example_loss_list[0][0])
+        best_pairs = temp_df.iloc[0][0]
         sen_sim_for_best_pairs = helpers.similarity_score(best_pairs[0], best_pairs[1])
-        attack_df.loc[len(attack_df)] = {"pairs": best_pairs, "loss": example_loss_list[best_idx][1], "sen_sim": sen_sim_for_best_pairs}
-        generated_df.loc[len(generated_df)] = {"pairs": best_pairs, "loss": example_loss_list[best_idx][1], "sen_sim": sen_sim_for_best_pairs}
+        attack_df.loc[len(attack_df)] = {"pairs": best_pairs, "loss": temp_df.iloc[0]["loss"], "sen_sim": temp_df.iloc[0]["sen_sim"]}
+        best_generated_df.loc[len(best_generated_df)] = {"pairs": best_pairs, "loss": example_loss_list[best_idx][1], "sen_sim": sen_sim_for_best_pairs}
+
 
       # normalize the loss TODO: clip the losses
       helpers.normalize_column(attack_df, "loss")
@@ -201,9 +217,10 @@ if __name__ == "__main__":
             starting_pair = attack_df.iloc[next_idx]["pairs"]
             break
 
+      print(used_pairs)
       used_pairs[starting_pair] = 1
 
     # best_attack_set = helpers.get_most_effective_pairs(args.set_size, attack_set)
     # helpers.append_dialogue_pairs_to_file(list(best_attack_set['dialogues']), args.output_path)
 
-    generated_df.to_csv("/content/hidden_bias_dialects/data/generated_attack_set.csv")
+    generated_df.to_csv("/content/hidden_bias_dialects/data/t5_large_generated_attack_set.csv")
